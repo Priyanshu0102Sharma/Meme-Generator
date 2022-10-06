63cc34c0d15f8005c4767cbbe53912575b08c7c8
Priyanshu <priyanshu0102here@gmail.com>
2022-10-06 12:49:10 +0530

    initial step for creating meme generating app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import logo from './assets/img1.png'
+import Info from './components/info'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+     
+       <Info />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/info.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../assets/img1.png'
+
+export default function Info(){
+    return (
+        <div>
+     
+        <img src={logo} width = "100px" />
+       <h4>Enjoy the meme</h4>
+     </div>
+    )
+}
